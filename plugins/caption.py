351e25f7f57830e135c0fb2027c556bf217fded5
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-19 21:13:36 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -35,7 +35,9 @@ async def caption(client, message: Message):
                 tz += "#چهارم"
             elif "5" in Jn:
                 tz += "#پنجم"
-            if X.__contains__("a") or X.__contains__("o") or X.__contains__("i") or X.__contains__("c") or X.__contains__("b") or X.__contains__("e") X.__contains__("l") or X.__contains__("n") or X.__contains__("m"):
+            elif "6" in Jn:
+                tz += "#ششم"
+            if X.__contains__("a") or X.__contains__("o") or X.__contains__("i") or X.__contains__("c") or X.__contains__("b") or X.__contains__("e") or X.__contains__("l") or X.__contains__("n") or X.__contains__("m"):
                 V = m.split("Bolum")[0]
                 E = V.split(f"{X}", -1)[0]
             else:
